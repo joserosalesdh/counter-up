2554c4209bf04ccae34ce2df90ea1d06ec50211f
joserosalesdh <jm.rosales92@gmail.com>
2020-08-20 14:45:49 -0300

    comit

---
+++ src/CounterApp.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({ value }) => {
+
+    // handleAdd
+    const handleAdd = (e) => {
+        console.log(e)
+    }
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2>{value}</h2>
+            <button onClick={handleAdd}>+1</button>
+        </>
+
+    )
+
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
+
+export default CounterApp;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+// import PrimeraApp from './PrimeraApp';
 import './index.css';
+import CounterApp from './CounterApp';
 
 
 
 const divRoot = document.querySelector('#root');
-
-ReactDOM.render(<PrimeraApp saludo='Hola Mundo' />, divRoot);
+{/* <PrimeraApp saludo='Hola Mundo' /> */ }
+ReactDOM.render(<CounterApp value={1} />, divRoot);
 
 
