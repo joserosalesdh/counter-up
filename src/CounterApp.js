49dea18cddfeea666d4e59462d1fdcafe5dd0fdf
joserosalesdh <jm.rosales92@gmail.com>
2020-08-20 15:16:19 -0300

    comit

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
 
+    // Un HOOK es una función
+    const [counter, setCounter] = useState(value);
+
     // handleAdd
-    const handleAdd = (e) => {
-        console.log(e)
+    const handleAdd = () => {
+        setCounter(counter + 1);
+        //setCounter((c) => c +1);
     }
+
+    const handleSubtract = () => {
+        setCounter(counter - 1);
+    }
+
+    const handleReset = () => {
+        setCounter(value)
+    }
+
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
             <button onClick={handleAdd}>+1</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleSubtract}>-1</button>
         </>
 
     )
